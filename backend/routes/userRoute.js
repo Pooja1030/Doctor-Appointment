23b2559d058330e008c6372637ec5d7527b00fbe
Pooja <poojasawant1030@gmail.com>
2024-11-07 15:52:40 +0530

    updated model and controller

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,15 @@
 import express from 'express'
-import { loginUser, registerUser } from '../controllers/userController.js'
+import { getProfile, loginUser, registerUser, updateProfile } from '../controllers/userController.js'
+import upload from '../middlewares/multer.js'
+import authUser from '../middlewares/authUser.js'
 
 const userRouter = express.Router()
 
 userRouter.post('/register',registerUser)
 userRouter.post('/login',loginUser)
+
+userRouter.get('/get-profile',authUser,getProfile)
+userRouter.post('/update-profile',upload.single('image'),authUser,updateProfile)
 
 
 
