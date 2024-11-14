8f068c50efd59d6bbfa00c12adacefc0e0203b6d
Pooja <poojasawant1030@gmail.com>
2024-11-14 14:40:03 +0530

    implemented appoinments section

--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -1,9 +1,14 @@
 import express from 'express'
-import { doctorList, loginDoctor } from '../controllers/doctorController.js'
+import { doctorList, loginDoctor, appointmentsDoctor, appointmentComplete, appointmentCancel } from '../controllers/doctorController.js'
+import authDoctor from '../middlewares/authDoctor.js'
+
 
 const doctorRouter = express.Router()
 
 doctorRouter.get('/list',doctorList)
 doctorRouter.post('/login',loginDoctor)
+doctorRouter.get('/appointments',authDoctor,appointmentsDoctor)
+doctorRouter.postt('/complete-appointment',authDoctor,appointmentComplete)
+doctorRouter.postt('/cancel-appointment',authDoctor,appointmentCancel)
 
 export default doctorRouter
