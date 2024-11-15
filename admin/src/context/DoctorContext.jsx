02080d803cca02c820929c716f16d6b65502bbe0
Pooja <poojasawant1030@gmail.com>
2024-11-15 10:47:11 +0530

    updated the appointments page

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -11,6 +11,8 @@ const DoctorContextProvider = (props) => {
 
     const {dToken, setDToken} = useState(localStorage.getItem('dToken')?localStorage.getItem('dToken'):'')
     const [appointments, setAppointments] = useState([])
+    const [dashData, setDashData] = useState(false)
+    const [profileData, setProfileData] = useState(false)
 
     const getAppointments = async () => {
         
@@ -64,9 +66,41 @@ const DoctorContextProvider = (props) => {
             console.log(error)
             toast.error(error.message)
         }
-
+        
     }
     
+    const getDashData = async () => {
+        try {
+
+            const {data} = await axios.get(backendUrl + '/api/doctor/dashboard', {headers:{dToken}})
+            if(data.success){
+                setDashData(data.dashData)
+                console.log(data.dashData)
+            } else {
+                toast.error(data.message)
+            }
+            
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+            
+        }
+    }
+    
+    const getProfileData = async () => {
+        try {
+            const {data} = await axios.get(backendUrl + '/api/doctor/profile', {headers:{dToken}})
+            if(data.success){
+                setProfileData(data.profileData)
+                console.log(data.profileData)
+            }
+            
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+            
+        }
+    }
     
     const value = {
         dToken, setDToken,
@@ -74,6 +108,8 @@ const DoctorContextProvider = (props) => {
         appointments,setAppointments,
         getAppointments,
         completeAppointment, cancelAppointment,
+        dashData,setDashData,getDashData,
+        profileData, setProfileData, getProfileData
     }
 
     return (
--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -1,6 +1,7 @@
 import express from 'express'
-import { doctorList, loginDoctor, appointmentsDoctor, appointmentComplete, appointmentCancel } from '../controllers/doctorController.js'
+import { doctorList, loginDoctor, appointmentsDoctor, appointmentComplete, appointmentCancel, doctorDashboard, updateDoctorProfile } from '../controllers/doctorController.js'
 import authDoctor from '../middlewares/authDoctor.js'
+import DoctorProfile from '../../admin/src/pages/Doctor/DoctorProfile.jsx'
 
 
 const doctorRouter = express.Router()
@@ -8,7 +9,10 @@ const doctorRouter = express.Router()
 doctorRouter.get('/list',doctorList)
 doctorRouter.post('/login',loginDoctor)
 doctorRouter.get('/appointments',authDoctor,appointmentsDoctor)
-doctorRouter.postt('/complete-appointment',authDoctor,appointmentComplete)
-doctorRouter.postt('/cancel-appointment',authDoctor,appointmentCancel)
+doctorRouter.post('/complete-appointment',authDoctor,appointmentComplete)
+doctorRouter.post('/cancel-appointment',authDoctor,appointmentCancel)
+doctorRouter.get('/dashboard', authDoctor, doctorDashboard)
+doctorRouter.get('/profile', authDoctor, DoctorProfile)
+doctorRouter.post('/update-profile', authDoctor, updateDoctorProfile)
 
 export default doctorRouter
